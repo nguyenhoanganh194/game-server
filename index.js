ae98a96aea93a40eab95f1ae3f371b21c9330cf5
Anh Nguyen <nguyenhoanganha1y@gmail.com>
2023-03-03 11:55:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.post("/ticket",(request, response) => {
       } 
       var ticketString = JSON.stringify(ticket)
       clients[clientId] = {
-        gui: username,
+        username: username,
         connection: null,
         time : time,
-        room : null
+        roomid : null
       }
       response.status(200).send(ticketString);
     }
@@ -52,7 +52,6 @@ app.post("/ticket",(request, response) => {
 
 
 wss.on('connection', (connection, req) => {
-  console.log(req.headers.token);
   if(req.headers.token != "valid_token"){
     connection.close(4500,"Wrong token");
   }
@@ -78,52 +77,21 @@ wss.on('connection', (connection, req) => {
       connection.close(4501,"Diff connect");
     }   
     var command = JSON.parse( data.toString());
-    console.log(command);
     switch(command.command){
-      case "Create":
-        if(clients[gui].room == null){
-          var room = {
-            white : gui,
-            black : null
-          }
-          var roomid = gui();
-          rooms[roomid] = room;
-          clients[gui].room = roomid;
-        }
+      case "CreateRoom":
+        CreateRoom(command,connection,gui);
         break;
-      case "Leave":
-        var room = rooms[clients[gui].room];
-        if( room != null){
-          if(room.black == gui){
-            room.black = null;
-          }
-          if(room.white == gui){
-            room.white = null;
-          }
-          if(room.black == null && room.white == null){
-            rooms[gui] = null;
-          }
-          clients[gui].room = null;
-        }
+      case "LeaveRoom":
+        LeaveRoom(command,connection,gui);
         break;
-      case "Join":
-        if(clients[gui].room == null){
-          var room = rooms[command.value.room];
-          if(room != null){
-            var isAdd = false;
-            if(room.white == null){
-              room.white = gui;
-              isAdd = true;
-            }
-            else if(room.black == null){
-              room.black = gui;
-              isAdd = true;
-            }
-            if(isAdd){
-              clients[gui].room = command.value.room;
-            }
-          }
-        }
+      case "JoinRoom":
+        JoinRoom(command,connection,gui);
+        break;
+      case "GetRooms":
+        GetRooms(command,connection,gui);
+        break;
+      case "GetMyRoomStatus":
+        GetMyRoomStatus(command,connection,gui);
         break;
     }
     
@@ -139,4 +107,114 @@ wss.on('connection', (connection, req) => {
 const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
 function S4() {
   return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+}
+
+
+function CreateRoom(command, connection, gui){
+  if(clients[gui].roomid == null){
+    var room = {
+      white : gui,
+      whiteReady : false,
+      black : null,
+      blackReady : false
+    }
+    var roomid = guid();
+    rooms[roomid] = room;
+    clients[gui].roomid = roomid;
+    console.log("New room created " + roomid);
+    var message = {
+      command: "CreateRoom",
+      value: "Accept"
+    }
+    connection.send(JSON.stringify(message));
+    return;
+  }
+  var message = {
+    command: "CreateRoom",
+    value: "Deny"
+  }
+  connection.send(JSON.stringify(message));
+}
+
+function LeaveRoom(command, connection, gui){
+  var room = rooms[clients[gui].roomid];
+  if( room != null){
+    if(room.black == gui){
+      room.black = null;
+    }
+    if(room.white == gui){
+      room.white = null;
+    }
+    if(room.black == null && room.white == null){
+      rooms[clients[gui].roomid] = null;
+    }
+    clients[gui].roomid = null;
+    var message = {
+      command: "LeaveRoom",
+      value: "Accept"
+    }
+    connection.send(JSON.stringify(message));
+  }
+}
+
+function JoinRoom(command, connection, gui){
+  var isAdd = false;
+  if(clients[gui].roomid == null){
+    var room = rooms[command.value];
+    if(room != null){
+      if(room.white == null){
+        room.white = gui;
+        isAdd = true;
+      }
+      else if(room.black == null){
+        room.black = gui;
+        isAdd = true;
+      }
+      if(isAdd){
+        clients[gui].roomid = command.value.room;
+      }
+    }
+  }
+  var message = {
+    command: "JoinRoom",
+    value: ""
+  }
+  if(isAdd){
+    message.value = "Accept";
+  }
+  else{
+    message.value = "Deny";
+  }
+  connection.send(JSON.stringify(message));
+}
+
+function GetRooms(command, connection, gui){
+  var message = {
+    command: "GetRooms",
+    value: JSON.stringify(rooms)
+  }
+  connection.send(JSON.stringify(message));
+}
+
+function GetMyRoomStatus(command, connection, gui){
+  var roomid = clients[gui].roomid;
+  console.log(roomid);
+  if(roomid != null){
+
+    var room = rooms[roomid];
+    console.log(room);
+    if(room != null){
+      var message = {
+        command: "GetMyRoomStatus",
+        value: JSON.stringify(room)
+      }
+      connection.send(JSON.stringify(message));
+      return;
+    }
+  }
+  var message = {
+    command: "GetMyRoomStatus",
+    value: "Deny"
+  }
+  connection.send(JSON.stringify(message));
 }
