13b4722821e9342f960f49d45394260b36969f53
Anh Nguyen <nguyenhoanganha1y@gmail.com>
2023-03-03 11:17:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.post("/ticket",(request, response) => {
       var time = Date.now();
       var ticket = {
         "token": "valid_token",
-        "gameserver": "http://127.0.0.1:" + wssPort.toString(),
+        "gameserver": "ws://127.0.0.1:" + wssPort.toString(),
         "username": username,
         "id" : clientId,
       } 
@@ -41,7 +41,8 @@ app.post("/ticket",(request, response) => {
       clients[clientId] = {
         gui: username,
         connection: null,
-        time : time
+        time : time,
+        room : null
       }
       response.status(200).send(ticketString);
     }
@@ -56,35 +57,84 @@ wss.on('connection', (connection, req) => {
     connection.close(4500,"Wrong token");
   }
   var gui = req.headers.gui;
-  if(clients[gui] == null){
-    connection.close(4500,"Wrong token");
-  } 
 
+  //Check gui and time stamp here
+  if(clients[gui] == null){
+    connection.close(4500,"Wrong gui");
+  } 
+  var accept = {
+    command: "Login",
+    value: "Accept"
+  }
+  connection.send(JSON.stringify(accept));
+  clients[gui].connection = connection;
 
   connection.on("open", () => console.log("opened!"))
   connection.on("close", () => console.log("closed!"))
 
   connection.on('message', function(data) {
-    if (typeof(data) === "string") {
-      // client sent a string
-      console.log("string received from client -> '" + data + "'");
-
-    } else {
-      console.log("binary received from client -> " + Array.from(data).join(", ") + "");
+    var gui = req.headers.gui;
+    if( clients[gui].connection != connection){
+      connection.close(4501,"Diff connect");
+    }   
+    var command = JSON.parse( data.toString());
+    console.log(command);
+    switch(command.command){
+      case "Create":
+        if(clients[gui].room == null){
+          var room = {
+            white : gui,
+            black : null
+          }
+          var roomid = gui();
+          rooms[roomid] = room;
+          clients[gui].room = roomid;
+        }
+        break;
+      case "Leave":
+        var room = rooms[clients[gui].room];
+        if( room != null){
+          if(room.black == gui){
+            room.black = null;
+          }
+          if(room.white == gui){
+            room.white = null;
+          }
+          if(room.black == null && room.white == null){
+            rooms[gui] = null;
+          }
+          clients[gui].room = null;
+        }
+        break;
+      case "Join":
+        if(clients[gui].room == null){
+          var room = rooms[command.value.room];
+          if(room != null){
+            var isAdd = false;
+            if(room.white == null){
+              room.white = gui;
+              isAdd = true;
+            }
+            else if(room.black == null){
+              room.black = gui;
+              isAdd = true;
+            }
+            if(isAdd){
+              clients[gui].room = command.value.room;
+            }
+          }
+        }
+        break;
     }
+    
   });
 
   connection.on('close', function() {
     console.log("client left.");
   });
-  if(clients[])
 
 });
 
-
-function onNewMessage(message){
-
-}
 
 const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
 function S4() {
